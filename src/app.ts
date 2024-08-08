7af5f60906a2e366db36e43d634a7dc11f1f1cc7
Polar0471 <rodrigoarth512@gmail.com>
2024-08-08 11:56:38 -0300

    feat: add CORS middleware to backend

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from "express"
 import router from "./routes"
+import cors from "cors"
 
 function createApp() {
     const app = express()
+    
+    const corsOptions = {
+        origin: 'http://localhost:3000',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true, 
+    }
 
     app.use(express.json())
+
+    app.use(cors(corsOptions));
 
     app.use ("/api/todos", router)
 
